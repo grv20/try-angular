f806a7ef6a09b77134c91fd0ebb7554a68db9731
Gaurav Arora <gaurav.arora@cantire.com>
2026-03-12 21:29:46 +0530

    directives

--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -2,10 +2,10 @@ import { Component, inject, signal } from '@angular/core';
 import { TodosService } from '../services/todos';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs/operators';
-
+import { TodoItem } from '../components/todo-item/todo-item';
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [TodoItem],
   templateUrl: './todos.html',
   styleUrl: './todos.scss',
 })
@@ -18,6 +18,17 @@ export class TodosComponent {
       return [];
     })).subscribe((todos) => {
       this.todoItems.set(todos);
+    });
+  }
+
+  updateTodoItem(todoItem: Todo) { 
+    this.todoItems.update((todos) => {
+      return todos.map((todo) => {
+        if (todo.id === todoItem.id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
     });
   }
 }
